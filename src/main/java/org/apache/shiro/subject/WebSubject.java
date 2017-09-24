7042ea6872b979ec409e5d1c481e4bbf52a33d4b
gejian <ge.j@qq.com>
2017-09-24 20:49:00 +0800

    bugfix:WebSubject#logout(HttpServletRequest) missed

--- src/main/java/org/apache/shiro/subject/WebSubject.java
+++ src/main/java/org/apache/shiro/subject/WebSubject.java
@@ -6,4 +6,11 @@ import java.io.Serializable;
 public interface WebSubject<T extends Serializable> extends Subject<T> {
 
     T getPrincipal(HttpServletRequest request);
+
+    /**
+     * logout current principal, which can be retrieved from the request
+     *
+     * @param request from which current principal can be retrieved
+     */
+    void logout(HttpServletRequest request);
 }
